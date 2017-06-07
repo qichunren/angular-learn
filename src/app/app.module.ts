2bbce968d09b85d51a948cf25d40fbc8eb0260bf
qichunren <whyruby@gmail.com>
2017-06-07 10:25:06 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 const appRoutes: Routes = [
-  { path: 'm1', loadChildren: './m1/m1.module#M1Module', },
-  { path: 'm2', loadChildren: './m2/m2.module#M2Module', }
+  { path: 'm1', loadChildren: "./m1/m1.module#M1Module" },
+  { path: 'm2', loadChildren: "./m2/m2.module#M2Module" }
   
 ];
 
--- src/app/m2/m2.module.ts
+++ src/app/m2/m2.module.ts
@@ -1,4 +1,7 @@
-import { NgModule } from '@angular/core';
+import { NgModule }       from '@angular/core';
+import { CommonModule }   from '@angular/common';
+import { FormsModule }    from '@angular/forms';
+
 import { M2Component } from './m2.component';
 
 @NgModule({
